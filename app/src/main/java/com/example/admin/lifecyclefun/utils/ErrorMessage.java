8538ae1d38a60e954334562f3509b53f2ead298c
victsomie <victsomie@yahoo.com>
2018-05-12 15:36:56 +0300

    Added network bound resource with all the dependent classes and dependencies

---
+++ app/src/main/java/com/example/admin/lifecyclefun/utils/ErrorMessage.java
@@ -0,0 +1,11 @@
+package com.example.admin.lifecyclefun.utils;
+
+/**
+ * Created by Admin on 2018-05-12.
+ */
+
+public class ErrorMessage {
+    public int StatusCode;
+    public String StatusDescription;
+    public String Message;
+}
---
+++ app/src/main/java/com/example/admin/lifecyclefun/utils/NetworkBoundResource.java
@@ -0,0 +1,51 @@
+package com.example.admin.lifecyclefun.utils;
+
+/**
+ * Created by Admin on 2018-05-12.
+ */
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
+import android.os.AsyncTask;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
+
+/**
+ *
+ * Some guideline of HOW TO USE THIS CLASS
+ * https://proandroiddev.com/build-an-app-with-offline-support-exposing-network-states-1e09e138b1ed
+ */
+
+// ResultType: Type for the Resource data
+// RequestType: Type for the API response
+public abstract class NetworkBoundResource<ResultType, RequestType> {
+    // Called to save the result of the API response into the database
+    @WorkerThread
+    protected abstract void saveCallResult(@NonNull RequestType item);
+
+    // Called with the data in the database to decide whether it should be
+    // fetched from the network.
+    @MainThread
+    protected abstract boolean shouldFetch(@Nullable ResultType data);
+
+    // Called to get the cached data from the database
+    @NonNull @MainThread
+    protected abstract LiveData<ResultType> loadFromDb();
+
+    // Called to create the API call.
+    @NonNull @MainThread
+    protected abstract LiveData<ApiResponse<RequestType>> createCall();
+
+    // Called when the fetch fails. The child class may want to reset components
+    // like rate limiter.
+    @MainThread
+    protected void onFetchFailed() {
+    }
+
+    // returns a LiveData that represents the resource, implemented
+    // in the base class.
+    public abstract LiveData<Resource<ResultType>> getAsLiveData();
+}
